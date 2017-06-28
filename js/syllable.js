a1b71dcc176ff6c68df3b33d96cccf95a35cd2be
Andrew Glines and David Wilson <andrew+david@>
2017-06-28 14:28:52 -0700

    successful call to syllable number

--- js/syllable.js
+++ js/syllable.js
@@ -1,27 +1,31 @@
 const datamuse = require('datamuse');
 var Bot = require('./../js/bot.js').botModule;
 
-  datamuse.request('words?sp=flower&md=s&max=1')
-  .then((json) => {
-      syllables = json[0].numSyllables;
-      newWord = json[0].word;
-      var currentSyllableCheck = new Bot();
-      currentSyllableCheck.checkSyllables(newWord, syllables);
-      console.log(newWord, syllables);
-    });
 
-
-
-// console.log("JSON dict length ", dictionary.length);
-
-var dictionary = JSON.parse('["one", "two", "OK", "four"]');
+var dictionary = JSON.parse('["one", "seven", "twenty", "unmitigated"]');
 
 function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+//========================================
 
-newRandomNumber = getRandomIntInclusive(1, dictionary.length-1);
-console.log("new rand # " + newRandomNumber);
-console.log(dictionary[newRandomNumber]);
+var newRandomNumber = getRandomIntInclusive(1, dictionary.length-1);
+var newRandomWord = dictionary[newRandomNumber];
+console.log("newRandomWord = " + newRandomWord);
+
+
+// wordRequest = "\'words?sp=" + newRandomWord + "&md=s&max=1\'";
+// console.log("wordRequest = " + wordRequest);
+
+
+
+  datamuse.request('words?sp=' + newRandomWord + '&md=s&max=1')
+  .then((json) => {
+      syllables = json[0].numSyllables;
+      newWord = json[0].word;
+      var currentSyllableCheck = new Bot();
+      currentSyllableCheck.checkSyllables(newWord, syllables);
+      console.log("newWord is " + newWord + " and syll# is ", syllables);
+    });
